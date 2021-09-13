40490063a9a24db75111bccdf4d02a1bacd07882
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-13 12:41:23 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux' /* connects a component to store's internal state to get data*/
 import './App.css';
 
+// in render and handleClick methods, store is no longer being passed directly to App
 class App extends Component {
 	handleOnClick = () => {
-		this.props.store.dispatch({
+		this.props.dispatch({
 			type: 'INCREASE_COUNT',
 		});
 	}
@@ -12,10 +14,16 @@ class App extends Component {
 		return (
 			<div className="App">
 				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
+				<p>{this.props.clicks}</p>
 			</div>
 		);
 	}
 }
 
-export default App;
+/*specify exactly which slice of the state we want to provide to our component. Here, we want to provide state.clicks, and allow our component to have access to them through a prop called clicks. We are then able to render the number of clicks in our render method using this.props.clicks */
+const mapStateToProps = (state) => {
+	return { clicks: state.clicks }
+}
+
+/*specify that we are connecting this state to the App component. In the end, the connect() method returns a new component which looks like the App component we wrote, but is connected up to receive the correct data. This new component is the component we wish to export. */
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux' /*(1) makes store available to nested components.
+(2) alerts redux app of any changes in state, which will re-render  react app */
 import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
@@ -10,4 +12,10 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ); 
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}> 
+    {/* store is now available to nested components */}
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
